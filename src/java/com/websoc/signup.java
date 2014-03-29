3491fcc0d7f82aabee96545c350420217c6a4108
Abhishek Solanki <abhisolanki94@gmail.com>
2014-03-29 23:47:30 +0530

    Added Signup template(singnup.html),signupDAO Remarks: Not final

---
+++ src/java/com/websoc/signup.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.websoc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.websoc.signupDAO;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Abhishek Solanki
+ */
+@WebServlet(name = "signup", urlPatterns = {"/signup"})
+public class signup extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String name,email,userName,password,bMonth,bDate,bYear,gender,mobile;
+        name=request.getParameter("name");
+        email=request.getParameter("email");
+        userName=request.getParameter("userName");
+        password=request.getParameter("password");
+        bDate=request.getParameter("bMonth");
+        bDate=request.getParameter("bDate");
+        bDate=request.getParameter("bYear");
+        gender=request.getParameter("gender");
+        mobile=request.getParameter("mobile"); 
+        
+        signupDAO signupObject = new signupDAO(name, email, userName, password, bDate, gender, mobile);
+       
+        try {
+            signupObject.execute();
+            System.out.println("Registration Completed Successfully: signup.java Servlet");
+        } catch (Exception ex) {
+            System.out.println(ex+"\n Error in Signup Servlets");
+        }
+        
+        
+              
+       
+        
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
---
+++ src/java/com/websoc/signupDAO.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.websoc;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.management.j2ee.statistics.JDBCConnectionPoolStats;
+/**
+ *
+ * @author Abhishek Solanki
+ */
+public class signupDAO {
+    
+    private String name,email,userName,password,bDate,gender,mobile,lastAccessed,IP;
+    String key = "Messaging";
+    String url = "jdbc:mysql://localhost:3306/";
+    String dbUser = "root";
+    String dbPass ="krish";
+    signupDAO(String name,String email,String userName,String password,String bDate,String gender,String mobile)
+    {
+        this.key = "messaginSystem";
+        this.name=name;
+        this.email=email;
+        this.userName=userName;
+        this.password=password;
+        this.bDate=bDate;
+        this.gender=gender;
+        this.mobile=mobile;
+    }
+
+    void execute() throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+       Class.forName("com.mysql.jdbc.Driver").newInstance();
+       Connection con = DriverManager.getConnection(url,dbUser,dbPass);
+       Statement stm = con.createStatement();
+       String query = "insert into messaging.main values(default,'"+userName+"','"+email+"',aes_encrypt('"+password+"','"+key+"'),"
+               + "'"+lastAccessed+"','"+IP+"','"+name+"','"+bDate+"','"+gender+"',default,'"+mobile+"');";
+       stm.executeUpdate(query);
+       System.out.println("Success in signupDAO");
+       
+    }      
+  
+        
+   
+}
